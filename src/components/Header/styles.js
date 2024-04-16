9c2aa3c9239a8abbc070d24f220635e0157d2aba
Rodrigo Matos <rodriguou2k20@gmail.com>
2024-04-16 18:24:16 -0300

    refactor: marginHorizontal retirado do header; paddingHorizontal adicionado à home

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,8 +2,7 @@ import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
     container: {
-        marginTop: 15,
-        marginHorizontal: 30,
+        marginTop: 15
     },
     text: {
         fontFamily: 'Archivo_700Bold'
