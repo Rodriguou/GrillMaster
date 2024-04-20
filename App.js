08eabef860e3d747f29ed1915041eac3dc631422
Rodrigo Matos <rodriguou2k20@gmail.com>
2024-04-20 18:04:09 -0300

    feat: fonte nova adicionada

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { useFonts, Archivo_400Regular, Archivo_600SemiBold, Archivo_700Bold } from '@expo-google-fonts/archivo'
+import { useFonts, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold, Archivo_700Bold, } from '@expo-google-fonts/archivo'
 
 import Routes from './src/routes'
 import Loading from './src/components/Loading'
 
 export default function App() {
-    const [fontsLoaded] = useFonts({ Archivo_400Regular, Archivo_600SemiBold, Archivo_700Bold })
+    const [fontsLoaded] = useFonts({ Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold, Archivo_700Bold })
 
     return(
         <NavigationContainer>
