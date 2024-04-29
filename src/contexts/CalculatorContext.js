1bb2b14e51d3503b5a064ec7fecd70ab957cc1e1
WevertonSPWOS <weverton.spwos@gmail.com>
2024-04-29 14:26:04 -0300

    refactor: Preço das carnes atualizados

--- src/contexts/CalculatorContext.js
+++ src/contexts/CalculatorContext.js
@@ -10,15 +10,15 @@ export default function CalculatorProvider({ children }) {
     })
 
     const meats = [
-        { name: 'Picanha', price: 35.00, type: 'beef', image: require('../../assets/coal.png') },
-        { name: 'Costela', price: 32.90, type: 'beef', image: require('../../assets/soda.png') },
-        { name: 'Maminha', price: 36.98, type: 'beef', image: require('../../assets/coal.png') },
-        { name: 'Lombinho', price: 40.53, type: 'pork', image: require('../../assets/coal.png') },
-        { name: 'Pernil', price: 32.99, type: 'pork', image: require('../../assets/coal.png') },
-        { name: 'Paleta', price: 50.00, type: 'pork', image: require('../../assets/coal.png') },
-        { name: 'Coxa', price: 30.00, type: 'chicken', image: require('../../assets/coal.png') },
-        { name: 'Asa', price: 35.96, type: 'chicken', image: require('../../assets/coal.png') },
-        { name: 'Coração', price: 80.74, type: 'chicken', image: require('../../assets/coal.png') }
+        { name: 'Picanha', price: 58.00, type: 'beef', image: require('../../assets/coal.png') },
+        { name: 'Costela', price: 27.00, type: 'beef', image: require('../../assets/soda.png') },
+        { name: 'Maminha', price: 43.00, type: 'beef', image: require('../../assets/coal.png') },
+        { name: 'Lombinho', price: 36.00, type: 'pork', image: require('../../assets/coal.png') },
+        { name: 'Pernil', price: 28.00, type: 'pork', image: require('../../assets/coal.png') },
+        { name: 'Paleta', price: 20.00, type: 'pork', image: require('../../assets/coal.png') },
+        { name: 'Coxa', price: 14.00, type: 'chicken', image: require('../../assets/coal.png') },
+        { name: 'Asa', price: 14.00, type: 'chicken', image: require('../../assets/coal.png') },
+        { name: 'Coração', price: 35.00, type: 'chicken', image: require('../../assets/coal.png') }
     ]
 
     const [selectedMeats, setSelectedMeats] = useState([])
